ade1e3bda4a9b979c5fd8b2fc94531c0d8f82845
ilkin1zahidov <ilkiniz@code.edu.az>
2022-06-23 02:26:29 +0400

    classname

---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace array_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = new int[10];
+            //numbers[0] = 5;
+            //numbers[1] = 10;
+            //numbers[2] = 100;
+
+
+            //Array.Resize(ref numbers, 20);
+            //numbers[12] = 95;
+            //foreach (var item in numbers) 
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //int[] nums = { 1, 2, 3, 6, 5, 8, 7, 11, 10 };
+            //Array.Sort(nums);
+
+            //foreach (var item in nums)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //int[,] arr = {
+            //    {1, 2},
+            //    {3, 4},
+            //    {5, 6 }
+            //};
+
+            //Console.WriteLine(arr[1,1]);
+
+            //string name1 = "Ilkin";
+            //string surname1 = "Zahidov";
+            //byte age1 = 24;
+
+            //string name2 = "Orxan";
+            //string surname2 = "Akberov";
+            //byte age2 = 26;
+
+            //string name3 = "Fidan";
+            //string surname3 = "Merdanli";
+            //byte age3 = 17;
+
+            //Console.WriteLine(name1 + " " + surname1);
+
+
+            //var obj1 = new
+            //{
+            //    name = "Ilkin",
+            //    surname = "Zahidov",
+            //    age = "24"
+            //};
+
+            //var obj2 = new
+            //{
+            //    name = "Orxan",
+            //    surname = "Akberov",
+            //    age = "26"
+            //};
+
+            //Console.WriteLine($"{obj1.name} {obj1.surname} Age: {obj1.age}");
+
+            //Student st1 = new Student(24,"Yeni Yasamal");
+            //st1.name = "Ilkin";
+            //st1.surname = "Zahidov";
+            //st1.age = 24;
+
+            //Student st2 = new Student();
+            //st2.name = "Orxan";
+            //st2.surname = "Akberov";
+            //st2.age = 26;
+
+            //Console.WriteLine(st1.FullName());
+            //Console.WriteLine(st1.FullData());
+
+            //Student student = new Student();
+            //student.name = "Ilkin";
+            //var result = student.GetGroupInfo();
+            //Console.WriteLine(result);
+
+        }
+
+        
+
+
+    }
+
+    class Student
+    {
+        //public string name;
+        //public string surname;
+        //public byte age;
+        //public string adress;
+        //public string FullName()
+        //{
+        //    return name + " " + surname;
+        //}
+        //public string FullData()
+        //{
+        //    return name + " " + surname + " " + age;
+        //}
+
+
+
+
+        //public Student(int age, string address) : this(address)
+        //{
+        //    Console.WriteLine("Age:" + age);
+        //}
+        //public Student(string adds):this()
+        //{
+        //    Console.WriteLine("address" + adds);
+        //}
+
+
+        //public Student() 
+        //{
+        //    Console.WriteLine("Empty");
+        //}
+
+        //public Student(bool isMarried) 
+        //{
+        //    Console.WriteLine("Married");
+        //}
+
+    //    public string GetGroupInfo() 
+    //    {
+    //     Teacher teacher = new Teacher();
+    //        teacher.name = "Cavid";
+    //        return $"Teacher name: {teacher.GetTeacherName()} Student name: {name}";
+    //    }
+
+    //}
+
+    //class Teacher
+    //{
+    //    public string name;
+    //    public decimal salary;
+
+
+    //    public string GetTeacherName() 
+    //    {
+    //        return name;
+    //    }
+    //}
+
+
+
+}
